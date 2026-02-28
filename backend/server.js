f198f803fd7095b0cae30fff71375dd2c3dd7683
pavan4 <izon2709@gmail.com>
2026-02-28 19:39:47 +0530

    Deployment Process

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(
       "http://localhost:5500",
       "http://127.0.0.1:5500",
       "http://localhost:3000",
+      "https://khata-book-two.vercel.app/login.html",
       /\.vercel\.app$/, // allows any vercel.app subdomain
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
